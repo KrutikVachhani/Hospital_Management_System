4d51c8a052b4676e73fb28a2d19e88459e38001b
KrutikVachhani <krutikvachhani23753@gmail.com>
2025-01-28 11:51:34 +0530

    First Commit

---
+++ frontend/src/Home.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+        <h1>Hello From My</h1>
+      <img className="png" alt="hello" src="https://www.darshan.ac.in/U01/PlacementNews/241/Kathan%20-%20Copy-Attachment-2022-07-15-103103.png" />
+      </>
+    )
+  }
+  
